47a41eac5cefdd764560d65079693e6f2a1499bb
Omar Baradei <omarbaradei21@gmail.com>
2017-01-29 16:44:11 -0500

    Completed method in Person5.java

--- M2/M2/src/main/java/edu/gatech/oad/antlab/person/Person5.java
+++ M2/M2/src/main/java/edu/gatech/oad/antlab/person/Person5.java
@@ -31,7 +31,12 @@ public class Person5 {
 	 */
 	private String calc(String input) {
 	  //Person 5 put your implementation here
-	  return null;
+        if (input == null) {
+            return null;
+        } else if (input.length() < 2) {
+            return input;
+        }
+        return input.substring(2) + input.substring(0, 2);
 	}
 	
 	/**
